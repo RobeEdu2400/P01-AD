7cece7df1bf4b1924b64de61260c4b66f22a5fe4
RobeEdu2400 <robe_edu@hotmail.com>
2017-05-04 16:39:14 +0200

    hola

--- p01/p01/Form1.Designer.vb
+++ p01/p01/Form1.Designer.vb
@@ -22,17 +22,297 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(34, 63)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(254, 452)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Empleados"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(326, 63)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(159, 84)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ordenar por:"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(7, 50)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(79, 21)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Apellido"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(7, 22)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(106, 21)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Identificador"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ComboBox1)
+        Me.GroupBox2.Controls.Add(Me.TextBox5)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(542, 63)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(420, 288)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Empleado"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Sin Jefe"})
+        Me.ComboBox1.Location = New System.Drawing.Point(178, 241)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(200, 24)
+        Me.ComboBox1.TabIndex = 11
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(178, 202)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(200, 22)
+        Me.TextBox5.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(178, 159)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(200, 22)
+        Me.TextBox4.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(178, 122)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(200, 22)
+        Me.TextBox3.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(178, 84)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(200, 22)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(178, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(200, 22)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 241)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 17)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Jefe:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 202)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(145, 17)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Fecha de Nacimiento:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 159)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(50, 17)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Cargo:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 17)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Nombre:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Apellidos:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "IdEmpleado:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(326, 216)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(159, 57)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Siguiente"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(326, 153)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(159, 57)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Cargar Datos"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(542, 357)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 84)
+        Me.ListBox2.TabIndex = 6
+        Me.ListBox2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(326, 284)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(159, 57)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Eliminar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(326, 347)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(159, 57)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Modificar"
+        Me.Button4.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1106, 523)
+        Me.ClientSize = New System.Drawing.Size(1045, 525)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
 End Class
--- p01/p01/Form1.vb
+++ p01/p01/Form1.vb
@@ -1,3 +1,148 @@
-﻿Public Class Form1
+﻿Imports System.Data
+Imports System.Data.SqlClient
 
+Public Class Form1
+    Private connection As SqlConnection
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RadioButton1.Checked = True
+        connection = New SqlConnection
+        connection.ConnectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=MAGATZEM;Trusted_Connection=True;"
+        Try
+            connection.Open()
+        Catch
+            MsgBox("problema en el servidor de base de datos")
+        End Try
+
+        If connection.State = ConnectionState.Closed Then
+            MsgBox("tanquem")
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cargarInformacion()
+
+    End Sub
+
+    Private Sub cargarInformacion()
+        Dim command As SqlCommand
+        Dim reader As SqlDataReader
+        Dim apellidos, nombre, idEmpleado As String
+
+        command = New SqlCommand
+        command.Connection = connection
+
+        If RadioButton1.Checked = True Then
+            command.CommandText = "select idEmpleado, Apellidos, Nombre,Cargo from Empleados order by idEmpleado"
+        Else
+            command.CommandText = "select idEmpleado, Apellidos, Nombre,Cargo from Empleados order by Apellidos"
+
+        End If
+
+        reader = command.ExecuteReader
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+
+        While reader.Read()
+            apellidos = reader("Apellidos").ToString
+            nombre = reader("Nombre").ToString
+            idEmpleado = reader("idEmpleado").ToString
+            ListBox1.Items.Add(nombre + "-" + apellidos)
+            ComboBox1.Items.Add(nombre + "-" + apellidos)
+            ListBox2.Items.Add(idEmpleado)
+        End While
+        reader.Close()
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim command, command1 As SqlCommand
+        Dim reader, reader1 As SqlDataReader
+        Dim posicion As Integer
+        Dim id As String
+        Dim jefe As String
+
+
+        posicion = ListBox1.SelectedIndex
+        id = ListBox2.Items.Item(posicion)
+
+        command = New SqlCommand
+        command.Connection = connection
+
+        command1 = New SqlCommand
+        command1.Connection = connection
+        command.CommandText = "select idEmpleado, Apellidos, Nombre,Cargo,FechaNacimiento,Jefe from Empleados where idEmpleado = " + id
+        reader = command.ExecuteReader
+
+
+        reader.Read()
+        TextBox1.Text = reader("idEmpleado").ToString
+        TextBox2.Text = reader("Apellidos").ToString
+        TextBox3.Text = reader("Nombre").ToString
+        TextBox4.Text = reader("Cargo").ToString
+        TextBox5.Text = reader("FechaNacimiento").ToString
+        jefe = reader("Jefe").ToString
+        MsgBox(jefe)
+        reader.Close()
+
+        If jefe IsNot "" Then
+            command1.CommandText = "select Apellidos, Nombre from Empleados where idEmpleado = " & Integer.Parse(jefe)
+            reader1 = command1.ExecuteReader
+            If reader1.Read() Then
+                For c As Integer = 1 To ComboBox1.Items.Count - 1
+                    If (ComboBox1.Items.Item(c).Equals(reader1("Nombre").ToString & "-" & reader1("Apellidos").ToString)) Then
+                        ComboBox1.SelectedIndex = c
+
+                    Else
+                        ComboBox1.SelectedIndex = 0
+                    End If
+                Next
+
+            End If
+            reader1.Close()
+        Else
+            ComboBox1.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            ListBox1.SelectedIndex = ListBox1.SelectedIndex + 1
+        Catch
+            MsgBox("No hay más empleados.")
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim command As SqlCommand
+
+        command = New SqlCommand()
+        command.Connection = connection
+        command.CommandText = String.Format("DELETE FROM Empleados where idempleado =" + TextBox1.Text)
+
+        If command.ExecuteNonQuery() = 0 Then
+            MsgBox("No se ha borrado ningun empleado")
+        Else
+            MsgBox("Empleado Borrado")
+            cargarInformacion()
+            ListBox1.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim command As SqlCommand
+
+        command = New SqlCommand()
+        command.Connection = connection
+        command.CommandText = String.Format("update empleados set apellidos = '{0}',nombre = '{1}',cargo = '{2}',FechaNacimiento = '{3}',jefe = '{4}' where idempleado = {5}", TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text, ComboBox1.SelectedIndex, TextBox1.Text)
+
+        If command.ExecuteNonQuery() = 0 Then
+            MsgBox("No se ha modificado la información")
+        Else
+            MsgBox("Midificacion realizada")
+            cargarInformacion()
+
+        End If
+    End Sub
 End Class
